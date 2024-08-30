99ea8ee21223062d44cc0909e3c403345dba124d
Luciano Gomes <lucianogvda02@gmail.com>
2024-08-29 21:41:04 -0300

    style: finish style of input component

--- src/components/FormInput/FormInput.tsx
+++ src/components/FormInput/FormInput.tsx
@@ -9,7 +9,11 @@ export default function FormInput({ label, inputType, placeholder }: InputProps)
         <section className="flex flex-col mb-4">
             <label className="mb-1 text-left text-text-primary">{label}</label>
             <input
-                className="w-80 h-12 p-2 border bg-background-primary text-text-secondary border-none rounded-lg" 
+                className="
+                    w-80 h-12 p-2 
+                    border shadow-sm
+                    focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent
+                     bg-background-primary text-text-secondary rounded-lg" 
                 type={inputType} 
                 placeholder={placeholder}
             />
